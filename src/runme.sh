c6cea65593d78e5f524e3c4a765a7f8619a706a8
Oriol <oriolninr@gmail.com>
2023-03-07 23:06:32 +0100

    Updated runme.sh script from ansible image

--- src/runme.sh
+++ src/runme.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
+echo "SSH agent configuration"
 eval `ssh-agent`
 ssh-add /home/ansible/rsa
-echo "Tot ok de moment"
 cd /opt/ansible
 export PATH=$PATH:/home/ansible/.local/bin
+echo "Installing docker collection"
 ansible-galaxy collection install community.docker
+echo "Running playbook with ansible-playbook -i inventory 00-install-web-servers.yml --diff"
 ansible-playbook -i inventory 00-install-web-servers.yml --diff
